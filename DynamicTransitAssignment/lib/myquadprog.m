0541944b7d82f6ebc26836342cb019beb2249454
Zhuang Qian <987791455@qq.com>
2025-10-04 17:47:01 +0800

    Delete DynamicTransitAssignment directory

--- DynamicTransitAssignment/lib/myquadprog.m
+++
@@ -1,7 +0,0 @@
-function [S,y] =myquadprog (H,f,A,b,Aeq,beq,lb,ub)
-opts = optimoptions('quadprog','Display','off');
-[S,y] = quadprog(H,f,A,b,Aeq,beq,lb,ub,[],opts)
-end
-%function [result] = myadd(a,b)
-% result = a + b;
-%end
